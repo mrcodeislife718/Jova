6c4c4481c929a293b16250bfd2661dea53b4d3f6
Charles Castillo <mrcodeislife718@proton.me>
2026-08-10 16:30:14 -0400

    Export JOVA 0.3 lossless editing APIs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 export { tokenize } from './tokenizer.js';
+export { tokenizeLossless, rawSlice, tokenAt } from './lossless.js';
 export { parse, parseValue } from './parser.js';
 export { stringify, serializeDocument } from './serializer.js';
 export { toJSON, fromJSON } from './convert.js';
 export { getNode, getMember, setValue, createValueNode } from './document.js';
+export {
+  replaceValue,
+  renameMember,
+  removeValue,
+  insertMember,
+  insertElement,
+  moveValue,
+} from './edit.js';
 export { JovaSyntaxError } from './errors.js';
