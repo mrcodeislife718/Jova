6d2253893ae1239aacfd50b7a194ffb7388845f8
Charles Castillo <mrcodeislife718@proton.me>
2026-08-10 16:24:10 -0400

    Export JOVA document AST utilities

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export { tokenize } from './tokenizer.js';
 export { parse, parseValue } from './parser.js';
 export { stringify, serializeDocument } from './serializer.js';
 export { toJSON, fromJSON } from './convert.js';
+export { getNode, getMember, setValue, createValueNode } from './document.js';
 export { JovaSyntaxError } from './errors.js';
