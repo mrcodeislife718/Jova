4bf6fdc2b70c8937ccfbd470aecbdf8eb23a1727
Charles Castillo <mrcodeislife718@proton.me>
2026-08-10 20:38:35 -0400

    Export resilient recovery parser and LSP server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { tokenize } from './tokenizer.js';
 export { tokenizeLossless, rawSlice, tokenAt } from './lossless.js';
 export { parse, parseValue } from './parser.js';
 export { parseTolerant, isRecoveryNode } from './recovery.js';
+export { parseRecovering } from './recovery-parser.js';
 export { stringify, serializeDocument } from './serializer.js';
 export { toJSON, fromJSON } from './convert.js';
 export { getNode, getMember, setValue, createValueNode } from './document.js';
@@ -35,4 +36,5 @@ export {
   renameEdits,
   recoverEditorDocument,
 } from './editor-intelligence.js';
+export { createLanguageServer } from './lsp-server.js';
 export { JovaSyntaxError } from './errors.js';
