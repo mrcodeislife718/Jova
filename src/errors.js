4f463c21c29172f43436b49b377586e3d6f6eeda
Charles Castillo <mrcodeislife718@proton.me>
2026-08-11 08:45:12 -0400

    Rename syntax error type to Scout

--- src/errors.js
+++ src/errors.js
@@ -1,8 +1,8 @@
-export class JovaSyntaxError extends SyntaxError {
+export class ScoutSyntaxError extends SyntaxError {
   constructor(message, position) {
     const suffix = position ? ` at ${position.line}:${position.column}` : '';
     super(`${message}${suffix}`);
-    this.name = 'JovaSyntaxError';
+    this.name = 'ScoutSyntaxError';
     if (position) {
       this.position = { ...position };
       Object.assign(this, position);
