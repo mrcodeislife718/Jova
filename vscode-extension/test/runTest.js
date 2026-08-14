2e225ace193adcff5684476a2ad9c14946844084
Charles Castillo <mrcodeislife718@proton.me>
2026-08-14 10:07:07 -0400

    Add Scout VS Code extension-host test runner

---
+++ vscode-extension/test/runTest.js
@@ -0,0 +1,17 @@
+const path = require('node:path');
+const { runTests } = require('@vscode/test-electron');
+
+async function main() {
+  const extensionDevelopmentPath = path.resolve(__dirname, '..');
+  const extensionTestsPath = path.resolve(__dirname, 'suite', 'index.js');
+  await runTests({
+    extensionDevelopmentPath,
+    extensionTestsPath,
+    launchArgs: ['--disable-extensions'],
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
